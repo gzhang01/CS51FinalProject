991c8d592de697c3b30bf8ef19a85074c474808e
Lwedlund <lwedlund@college.harvard.edu>
2015-04-16 22:34:47 -0400

    new classes added

---
+++ robot.py
@@ -0,0 +1,18 @@
+class Robot:
+  def_init_(self, x, y, r):
+  self.x = x
+  self.y = y
+  self.r = r
+
+  def getX(self) :
+  	return self.x
+
+  def getY(self) :
+  	return self.y
+
+  def getRadius(self) :
+  	return self.r
+
+  def move(x1 y1)
+    self.x = x1
+    self.y = y1
