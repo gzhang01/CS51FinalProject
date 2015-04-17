64b988979cd7267d3806f9d56828c40fc0c2787e
ziziberry <zizi.z.96@gmail.com>
2015-04-16 22:21:08 -0400

    Added node.py

---
+++ node.py
@@ -0,0 +1,28 @@
+class Node: 
+	def _init_(self, x, y, neighbors):
+	self.x = x
+	self.y = y
+	#neighbors is a list of other points near this node that are also nodes
+	self.neighbors = neighbors 
+
+	def get_x(self) :
+		return self.x
+
+	def get_y(self): 
+		return self.y
+
+	def get_dist(self, other):
+		return ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+		
+	def get_neighbors(self):
+		return self.neighbors
+
+	def add_neighbor(self, other):
+		neighbors.append(other)
+
+	def remove_neighbor(self, other):
+		neighbors.remove(other)
+
+	#def getStatus(self)
+
+
