21564c8f94bb336a7fc4048877ec8ea6862ce03c
ziziberry <zizi.z.96@gmail.com>
2015-04-16 22:33:34 -0400

    Node.py interface done

--- node.py
+++ node.py
@@ -5,25 +5,25 @@ class Node:
         # neighbors is a list of other points near this node that are also nodes
         self.neighbors = neighbors
         
-        def get_x(self) :
-            return self.x
+    def get_x(self) :
+        return self.x
         
-        def get_y(self):
-            return self.y
+    def get_y(self):
+        return self.y
         
-        def get_dist(self, other):
-            return ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+    def get_dist(self, other):
+        return ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
         
-        def get_neighbors(self):
-            return self.neighbors
+    def get_neighbors(self):
+        return self.neighbors
         
-        def add_neighbor(self, other):
-            neighbors.append(other)
+    def add_neighbor(self, other):
+        neighbors.append(other)
         
-        def remove_neighbor(self, other):
-            neighbors.remove(other)
+    def remove_neighbor(self, other):
+        neighbors.remove(other)
         
-        def obstacles(self):
-            # returns whether there is an obstacle between two Nodes
+    def obstacles(self):
+        # returns whether there is an obstacle between two Nodes
 
 
